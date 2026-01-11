92538a66a5f37cfd50201d6d1466d35430b28020
root <root@DESKTOP-FG8MUDL.localdomain>
2026-01-11 23:03:05 +0300

    uuppp

--- Julia/declare_ju.c
+++ Julia/declare_ju.c
@@ -3,11 +3,11 @@
 void declare_jvaribles(t_fractal *f)
 {
     f->min_re = -2.0;
-    f->max_re = 1.0;
-    f->min_im = -1.2;
-    f->max_im = 1.5;
+    f->max_re = 2.0;
+    f->min_im = -2.0;
+    f->max_im = 2.0;
     f->zoom = 1.0;
 
-    f->julia_cr = -0.285;
-    f->julia_ci = 0.01;
+    // f->julia_cr = -0.7;
+    // f->julia_ci = 0.27015;
 }
--- Julia/draw_julia.c
+++ Julia/draw_julia.c
@@ -3,10 +3,13 @@
 int    julia(double zr, double zi, t_fractal *f)
 {
     double temp;
-    int i = 0;
+    int i;
 
-    while (zr * zr + zi * zi <= 4 && i < MAX_ITER)
+    i = 0;
+    while (i < MAX_ITER)
     {
+        if (zr * zr + zi * zi > 4)
+            break ;
         temp = zr * zr - zi * zi + f->julia_cr;
         zi = 2 * zr * zi + f->julia_ci;
         zr = temp;
@@ -22,15 +25,19 @@ void    draw_julia(t_fractal *f)
     double zr;
     double zi;
     int i;
+    double re_factor;
+    double im_factor;
 
+    re_factor = (f->max_re - f->min_re) / WIDTH;
+    im_factor = (f->max_im - f->min_im) / HEIGHT;
     y = 0;
     while (y < HEIGHT)
     {
         x = 0;
+        zi = f->max_im - y * im_factor;
         while (x < WIDTH)
         {
-            zr = f->min_re + x * (f->max_re - f->min_re) / WIDTH;
-            zi = f->max_im - y * (f->max_im - f->min_im) / HEIGHT;
+            zr = f->min_re + x * re_factor;
             i = julia(zr, zi, f);
             pixel(f, x, y, get_color(i));
             x++;
